217e78711a01bf727e9bb0878b67a1e5d8c1708e
Kartel West <kartelwest007@gmail.com>
2026-07-21 02:52:06 -0300

    found 2 more changes that needed to be made

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -6,7 +6,7 @@ export async function createClient() {
 
   const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
   const supabaseKey =
-    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
   if (!supabaseUrl || !supabaseKey) {
     throw new Error("Supabase environment variables are missing.");
